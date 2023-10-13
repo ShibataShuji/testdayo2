e6e84ced1c7ed5c318773d51a41adcb1025351d9
ShibataShuji <0302shuji@gmail.com>
2023-10-13 17:02:39 +0900

    mokkai

--- Source/MyProject/Private/MyActor.cpp
+++ Source/MyProject/Private/MyActor.cpp
@@ -3,7 +3,7 @@
 
 #include "MyActor.h"
 
-// Sets default values
+// Sets default values aa
 AMyActor::AMyActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
